ae2afd1011b0e945b4e4ee7a1af381f77f30a3cc
zynque <zynque@github.com>
2024-12-21 21:59:12 -0800

    further development of pairing and propagation of state - example with two counter components feeding into a display of the combined total

--- src/main/scala/com/fractaltreehouse/threetyrz/examplecomponents/SideBySideExample.scala
+++ src/main/scala/com/fractaltreehouse/threetyrz/examplecomponents/SideBySideExample.scala
@@ -18,9 +18,24 @@ object SideBySideExample2:
   val b1 = CounterButton2.Component("Inc A")
   val b2 = CounterButton2.Component("Inc B")
   val paired = b1.pairWith(b2, (a, b) => div(a.map(Left(_)), b.map(Right(_))))
-  val withTotalAsString = paired.mapOutput {
-    case Left(a) => a.toString()
-    case Right(b) => b.toString()
+  val accumulated = paired.feedInto(EitherAccumulator())
+  val withTotalAsString = accumulated.mapOutput {
+    case (a, b) => (a.getOrElse(0) + b.getOrElse(0)).toString
   }
   val label = Label.Component("(total)")
   val component = withTotalAsString.feedInto(label, (a, b) => div(a.map(Left(_)), b.map(Right(_))))
+
+class EitherAccumulator[F[_], A, B] extends TyrianComponent2[F, Either[A, B], (Option[A], Option[B]), Nothing, (Option[A], Option[B])] {
+  def init: ((Option[A], Option[B]), Cmd[F, Nothing]) = ((None, None), Cmd.None)
+  def update(state: (Option[A], Option[B]), value: Either[Either[A, B], Nothing]): ((Option[A], Option[B]), Cmd[F, Either[(Option[A], Option[B]), Nothing]]) =
+    value match
+      case Left(Left(a)) =>
+        val newState = (Some(a), state._2)
+        (newState, Cmd.emit(Left(newState)))
+      case Left(Right(b)) =>
+        val newState = (state._1, Some(b))
+        (newState, Cmd.emit(Left(newState)))
+      case _ => (state, Cmd.None)
+        
+  def view(state: (Option[A], Option[B])): Html[Nothing] = div()
+}
